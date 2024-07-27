8489262887208c885fedb9a5d0936be94a1dac46
yhyoon1004 <yhyoon1004@github.com>
2024-07-27 23:32:06 +0900

    초기 프로젝트 설정

---
+++ src/main/java/yh_project/pageeventcollector/PageEventCollectorApplication.java
@@ -0,0 +1,13 @@
+package yh_project.pageeventcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageEventCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PageEventCollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yh_project/pageeventcollector/PageEventCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package yh_project.pageeventcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageEventCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
